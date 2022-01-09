f01343b3818ddf0d747f8382beb86811b65c92a7
Muhammad Zain <muhammadzainulabdin123@gmail.com>
2022-01-09 17:58:30 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import './App.css';
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      App
-    </div>
-  );  
+    <BrowserRouter>
+      <Routes>
+        <Route>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
