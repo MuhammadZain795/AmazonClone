565d3257575246a2acadbd120e699fe2d786a3ae
Muhammad Zain <muhammadzainulabdin123@gmail.com>
2022-01-09 18:41:45 +0500

    logo added

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../css/header.css';
+export default function Header() {
+    return (
+        <nav className='header'>
+            <img 
+                className='header_logo'
+                src="https://www.freepnglogos.com/uploads/amazon-png-logo-vector/large-images-amazon-png-logo-vector-7.png3ft3d1416935166817"
+            />
+        </nav>
+    )
+}
