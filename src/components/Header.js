5e11bd93e5a0b4f7319dbb0e161e3afb7e59c975
Muhammad Zain <muhammadzainulabdin123@gmail.com>
2022-01-09 22:24:44 +0500

    .

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import '../css/header.css';
+import SearchIcon from '@mui/icons-material/Search'
+
 export default function Header() {
     return (
         <nav className='header'>
-            <img 
-                className='header_logo'
-                src="https://www.freepnglogos.com/uploads/amazon-png-logo-vector/large-images-amazon-png-logo-vector-7.png3ft3d1416935166817"
-            />
+            <Link to="/">
+                <img 
+                    className='header_logo'
+                    src="https://www.freepnglogos.com/uploads/amazon-png-logo-vector/large-images-amazon-png-logo-vector-7.png3ft3d1416935166817"
+                />
+            </Link>
+            <input type="text" className='header_searchInput'/>
+            
         </nav>
     )
 }
