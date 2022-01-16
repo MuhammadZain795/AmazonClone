32955d3ba2a9fe854bad9561f05d54b8a9b4fada
Muhammad Zain <muhammadzainulabdin123@gmail.com>
2022-01-16 16:28:00 +0500

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import SearchIcon from '@material-ui/icons/Search';
 import '../css/header.css';
 
 export default function Header() {
@@ -11,8 +12,13 @@ export default function Header() {
                     src="https://www.freepnglogos.com/uploads/amazon-png-logo-vector/large-images-amazon-png-logo-vector-7.png3ft3d1416935166817"
                 />
             </Link>
-            <input type="text" className='header_searchInput'/>
-            
+            <div className='header_search'>
+                <input type="text" className='header_searchInput'/>
+                <SearchIcon className='hearder_serachIcon'/>
+            </div>
+            <div className='header_nav'>
+                <Link></Link>
+            </div>
         </nav>
     )
 }
