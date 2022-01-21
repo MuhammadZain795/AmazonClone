3d94fed60fb153a645338d88bb83a83c2d778369
Muhammad Zain <muhammadzainulabdin123@gmail.com>
2022-01-21 11:19:06 +0500

    banner added in home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,14 @@
 import React from 'react'
-import Header from '../components/Header'
+import '../css/home.css';
 
 export default function Home() {
   return (
-    <div>
-      Home
+    <div className='home'>
+      <img
+        className='home_image'
+        src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+        alt="banner image"
+      />
     </div>
     
   )
