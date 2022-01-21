3c216e24fbd6d6642846d2d9ab4f0f12007f138e
Muhammad Zain <muhammadzainulabdin123@gmail.com>
2022-01-21 10:42:51 +0500

    routing updtaed

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from './pages/Login';
 import Checkout from './pages/Checkout';
+import Header from './components/Header';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route>
-          <Route index element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/checkout" element={<Checkout />} />
+          <Route index element={[<Header/>, <Home />]} />
+          <Route path="/login" element={[<Header/>, <Login />]} />
+          <Route path="/checkout" element={[<Header/>,<Checkout/>]} />        
         </Route>
       </Routes>
     </BrowserRouter>
