398f081cf964d1aef01cbd3354d04d65c22f272b
Muhammad Zain <muhammadzainulabdin123@gmail.com>
2022-01-21 16:23:59 +0500

    product updated

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,20 +4,20 @@ import '../css/product.css';
 export default function Product({id, title, price, rating, image}) {
   return (
     <div className='product'>
-      <p>{title}</p>
-      <p className='product_price'>
-        <small>$</small>
-        <strong>{price}</strong>
-      </p>
-      <div className='product_rating'>
-          {
-              Array(rating)
-              .fill()
-              .map((_)=>{
-                  <p>⭐</p>
-              })
-          }
-      </div>
+        <div className='product_info'>
+            <p>{title}</p>
+                <p className='product_price'>
+                    <small>$</small>
+                    <strong>{price}</strong>
+                </p>
+            <div className='product_rating'>
+                {Array(rating).fill().map((_) => {
+                    <p>⭐</p>
+                })}
+            </div>
+        </div>
+        <img src={image}/>
+        <button>Add to Basket</button>
     </div>
     
   )
